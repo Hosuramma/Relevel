025db4e4f187820c0f29ba5e184dd4b4f101a3ac
Hosuramma <108606758+Hosuramma@users.noreply.github.com>
2022-07-11 18:39:31 +0530

    Add files via upload

---
+++ 2darr.js
@@ -0,0 +1,14 @@
+let $2darr = [
+    [1, 2, 3,4],
+    [5,6,7,8],
+    [9,0,2,1],
+    [1,1,1,1]
+];
+
+for (let i = 0;i<=$2darr.length-1;i++)  {
+    
+    for (let j=0;j<=$2darr.length-1;j++){
+        console.log($2darr[i][j] *5);
+        
+    }
+}
